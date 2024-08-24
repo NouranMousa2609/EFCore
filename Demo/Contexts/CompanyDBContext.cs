325c52596742d04c62818b3699b90996bb213cf8
NouranMousa2609 <nouranmousa55@gmail.com>
2024-08-24 14:10:57 +0300

    Part 02 Mapping Ways - 3. Fluent APIs

---
+++ Demo/Contexts/CompanyDBContext.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using CommonEntities;
+using Demo.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo.Contexts
+{
+    internal class CompanyDbContext : DbContext
+    {
+        //public CompanyDbContext():base()
+        //{
+
+        //}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+         => optionsBuilder.UseSqlServer("Server=DESKTOP-5P13SE6\\SQLEXPRESS;Database=Company;Trusted_Connection=True;Encrypt=False ");
+
+
+
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>()
+                  .Property("Address")
+                  .HasDefaultValue("Cairo");
+            modelBuilder.Entity<Department>().ToTable("Department", "dbo").HasKey(D => D.Id);
+            modelBuilder.Entity<Department>().Property(D => D.Id).UseIdentityColumn(10, 10);
+
+            modelBuilder.Entity<Department>()
+                .Property(D => D.Name)
+                .HasColumnName("DepartmentName")
+                .HasColumnType("varchar")
+                .HasMaxLength(50);
+            //.HasAnnotation("MaxLength",50)
+            //.HasAnnotation("",)
+            //.HasAnnotation("",)
+            //.HasAnnotation("",)
+            //.HasAnnotation("",)
+
+
+
+            modelBuilder.Entity<Department>()
+               .Property(D => D.CreationDate)
+               .HasComputedColumnSql("GETDATE()");
+            //.HasDefaultValue(DateOnly.FromDateTime(DateTime.Now));
+            //.HasColumnType("date")
+        }
+
+        public DbSet<Department> Departments { get; set; }
+
+    }
+}
---
+++ Demo/Entities/Employee.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Entities
+{
+    //EF support 4 ways for mapping
+    //1.by Convension
+    //2.Data Annotation
+    //3.fluent APIs ->DBContext:Override OnModelCreating()
+
+
+    ///Poco Class
+    ///Plain Old C# (CLR) object
+
+    //1.by Convension
+    //internal class Employee
+    //{
+    //    public int Id { get; set; }         // Public numeric property named "Id" || "EmployeeId" -->PK[Identity]
+
+    //    public string? Name { get; set; }    //string->nvarchar[max] //Reference type :Allow null[optional]
+
+    //    public double Salary { get; set; }  //value type     : not allow null[required]
+
+    //    public int? Age { get; set; }       // nullable<int> :Allow null [optional]
+    //}
+
+    //2.Data Annotation
+
+    internal class Employee
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]//(1,1)
+        public int Id { get; set; }
+        [Required]
+        [Column(TypeName = "varchar")]
+        [MaxLength(50)]
+        [StringLength(50, MinimumLength = 10)]
+
+        public string? Name { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "money")]
+        public double Salary { get; set; }
+
+        [Range(22, 60)]
+        public int? Age { get; set; }
+
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Phone]
+        [DataType(DataType.PhoneNumber)]
+        public string? PhoneNumber { get; set; }
+
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+
+    }
+
+
+}
